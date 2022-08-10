9b7e574b5e9432a002e779b8ae46713c7ae5fc98
Filin <nastia-dogf@mail.ru>
2022-08-10 14:11:19 +0300

    Added enum for network errors

---
+++ MVC-N/Application/Model/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  MVC-N
+//
+//  Created by Anastasia Perminova on 10.08.2022.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case failInternetError
+    case noInternetConnection
+}
