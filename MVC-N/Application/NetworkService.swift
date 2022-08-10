e2b4746ccb001d132182c98299af375f84dd3e4b
Filin <nastia-dogf@mail.ru>
2022-08-10 13:34:42 +0300

    Added method getData

--- MVC-N/Application/NetworkService.swift
+++ MVC-N/Application/NetworkService.swift
@@ -14,4 +14,18 @@ class NetworkService {
     private init() {
         
     }
+    
+    public func getData(url: URL, completion: @escaping (Any) -> ()) {
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
 }
