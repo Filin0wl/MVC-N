e97df21c7b3be258a9082b85b4e1c6059f48fc71
Filin <nastia-dogf@mail.ru>
2022-08-10 14:22:04 +0300

    Set init for GetCommentResponse

--- MVC-N/Comments/Model/Responses/GetCommentResponse.swift
+++ MVC-N/Comments/Model/Responses/GetCommentResponse.swift
@@ -9,4 +9,16 @@ import Foundation
 
 struct GetCommentResponse {
     
+    let comments: [Comment]
+    
+    init(json: Any) throws {
+        guard let array = json as? [[String: AnyObject]] else { throw NetworkError.failInternetError }
+        
+        var comments = [Comment]()
+        for dictionary in array {
+            guard let comment = Comment(dictionary: dictionary) else { continue }
+            comments.append(comment)
+        }
+        self.comments = comments
+    }
 }
